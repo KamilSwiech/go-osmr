cb38207825a7b132de8a5ef9003ee0426f8476e9
KamilSwiech <236389@student.pwr.edu.pl>
2020-07-29 19:32:56 +0200

    Add sorting and query validation

--- handler.go
+++ handler.go
@@ -3,44 +3,43 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"net/url"
-	"strings"
 	"fmt"
 	"log"
 )
 
 func main() {
 	http.HandleFunc("/routes", func(w http.ResponseWriter, r *http.Request) {
-		query := r.URL.Query()
-		src := query["src"]
-		dst := query["dst"]
-		if len(src) == 0 || len(dst) == 0 {
-			log.Println("No arguments")
+		if err := ValidateQuery(r); err != nil {
+			log.Println(err)
 			return
 		}
-		u, _ := url.Parse("http://router.project-osrm.org/route/v1/driving/")
-		path := append(src, dst...)
-		s := strings.Join(path, ";")
-		rel, _ := u.Parse(s)
-		log.Println(rel.String())
-		resp, _ := http.PostForm(rel.String(), url.Values{"overview":{"false"}})
-		body, _ := ExtractBody(resp)
-		log.Println(string(body))
+		node := RequestNode(r)
+	        fmt.Println(node)
 		w.WriteHeader(200)
-		fmt.Fprintf(w, "Hello, %q", src)
+		fmt.Fprintf(w, "Hello, %q", "HI")
 	})
 
 	resp, _ := GetRequest("http://router.project-osrm.org/route/v1/driving/13.388860,52.517037;13.397634,52.529407?overview=false")
 	body, _ := ExtractBody(resp)
-	var node Node
-	json.Unmarshal(body, &node)
-	fmt.Println(node)
+	var extNode OSMRResponse
+	json.Unmarshal(body, &extNode)
+	fmt.Println(extNode)
         // starting WWW server
         if err := http.ListenAndServe(":8080",nil); err != nil {
                 log.Fatal("ListenAndServe:",err)
         }  
 }
 
-func ParseToNode(ext ExtendedNode) (node Node){
-	
+// Create Node from OSMRResponse (OSMR response), source and destination array.
+// For proper work destination array order should correspond to routes order.
+func ParseToNode(osmr OSMRResponse, src []string, dst []string) (node Node){
+	node = Node{Source: src[0], Edge: make([]Edge, len(osmr.Routes))}
+	for idx, val := range osmr.Routes {
+		node.Edge[idx] = Edge{
+			Destination: dst[idx], 
+			Duration: val.Duration, 
+			Distance: val.Distance,
+		}
+	}
+	return node
 }
--- osmrApi.go
+++ osmrApi.go
@@ -1,5 +1,68 @@
 package main
 
+import (
+	"net/http"
+	"errors"
+	"strings"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/url"
+	"sort"
+)
+
+// Validate request query under OSMR restrictions
+func ValidateQuery (r *http.Request) error {
+	query := r.URL.Query()
+	src, dst := query["src"], query["dst"]
+	if len(src) == 0 || len(dst) == 0 {
+		return  errors.New("Not enough arguments specified")
+	} else if len(src) > 1 {
+		return  errors.New("Only one source is available")
+	}
+	return nil
+}
+
+// Make call to OSMR API and return Node struct
+func RequestNode (r *http.Request) *Node {
+        query := r.URL.Query()
+        src := query["src"]
+        dst := query["dst"]
+        u, _ := url.Parse("http://router.project-osrm.org/route/v1/driving/")
+        path := append(src, dst...)
+        s := strings.Join(path, ";")
+        rel, _ := u.Parse(s)
+        log.Println(rel.String())
+	resp, _ := http.PostForm(rel.String(), url.Values{"overview":{"false"}})
+        body, _ := ExtractBody(resp)
+        var osmr OSMRResponse
+        json.Unmarshal(body, &osmr)
+        fmt.Println(osmr)
+        node := ParseToNode(osmr, src, dst)
+	node.sortEdges()
+	return &node
+}
+
+// Sort egdes in node from smallest distance to highest
+func (node *Node) sortEdges(){
+	sort.Sort(ByTimeAndDistance(node.Edge)) 
+}
+
+type ByTimeAndDistance []Edge
+
+func (a ByTimeAndDistance) Len() int { return len(a) }
+
+func (a ByTimeAndDistance) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+
+func (a ByTimeAndDistance) Less(i, j int) bool {
+	if a[i].Duration == a[j].Duration {
+		return a[i].Distance < a[j].Distance
+	} else { 
+		return a[i].Duration < a[j].Duration
+	}
+}
+
+
 // Struct used to marshal data for "/routes" response
 type Node struct {
 	Source string   `json:"source"`
@@ -13,7 +76,7 @@ type Edge struct {
 
 
 // Struct used to unmarshal json from OSMR
-type ExtendedNode struct {
+type OSMRResponse struct {
 	Routes    []Routes    `json:"routes"`
 	Waypoints []Waypoints `json:"waypoints"`
 	Code      string      `json:"code"`
